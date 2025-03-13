0f4c78dcea473395b17eabba503fef0167433350
boyuan <531237512@outlook.com>
2025-03-12 20:07:03 -0400

    feat: playground done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Typography, Table, Button, Flex } from 'antd';
+
+const { Title } = Typography;
+
+// data = {
+//   "success": true,
+//   "data": [
+//     {
+//       "name": "Alice Johnson",
+//       "age": 20,
+//       "studentId": "S001",
+//       "message": "Welcome to the university!"
+//     },
+//     {
+//       "name": "Bob Smith",
+//       "age": 22,
+//       "studentId": "S002",
+//       "message": "Looking forward to the semester!"
+//     }
+//   ]
+// }
 
 function App() {
-  const [mockData, setMockData] = useState();
+  const [data, setData] = useState();
 
-  useEffect(() => {
-    console.log('Fetching /api/getUserName');
-    fetch('/api/getUserName')
+  const handleSearch = () => {
+    console.log('Fetching /api/example');
+    fetch('/api/example')
       .then((response) => response.json())
       .then((data) => {
-        setMockData(data);
+        setData(data);
       })
       .catch((error) => console.error('Error fetching mock data:', error));
-  }, []);
+  };
+
+  const columns = [
+    {
+      title: 'Student ID',
+      dataIndex: 'studentId',
+      key: 'studentId',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Message',
+      dataIndex: 'message',
+      key: 'message',
+    },
+  ];
 
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Cooker Example App</h1>
-      <h2>API Response</h2>
-      {mockData ? <div>{JSON.stringify(mockData, null, 2)}</div> : 'Loading...'}
+      <Title level={1}>Cooker Example App</Title>
+      <Title level={2}>API Response Data</Title>
+      {data ? <div>{JSON.stringify(data, null, 2)}</div> : 'Null'}
+
+      <Flex align="center" justify="space-between">
+        <Title level={2}>Student Info</Title>
+        <Button type="primary" onClick={handleSearch}>
+          Search
+        </Button>
+      </Flex>
+      <Table dataSource={data?.data} columns={columns} />
     </div>
   );
 }
